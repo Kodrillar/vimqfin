4e73516fde7f2305e4bef870a192201463da2366
Bonzerbay <bonzerbay@gmail.com>
2022-07-09 12:22:17 +0100

    changed app title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
        
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Home'),
     );
   }
 }
